c5bfa86166279dd9de1826f5cd5f3a53b26f6bf1
INSA-Corella <corella@insa-toulouse.fr>
2022-11-17 12:15:50 +0100

    UDPListener first implementation MaJ add pseudo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;  // Import the Scanner class
 
 /**
  * Hello world!
@@ -17,9 +18,14 @@ public class App
         UDPListener recep = new UDPListener();
         recep.start();
 
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter pseudo : ");
+
+        String pseudo = myObj.nextLine();  // Read user input
+
         System.out.println( "Hello World!" );
         DatagramSocket dgramSocket = new DatagramSocket();
-        String message = "connexion";
+        String message = "connexion " +pseudo;
         InetAddress broadcast = InetAddress.getByName("10.1.5.224");
         int port = 4445;
         DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(),broadcast, port);
