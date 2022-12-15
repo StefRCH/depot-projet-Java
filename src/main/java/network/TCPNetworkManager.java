ce97396eb2ffc5c20d4c256460ee36144ce701e1
Toto <mmartin6@insa-20669>
2022-12-15 11:58:44 +0100

    Ajout de code dans les classes TCPNetworkManager et TransmitterThread

--- src/main/java/network/TCPNetworkManager.java
+++ src/main/java/network/TCPNetworkManager.java
@@ -6,8 +6,31 @@ import java.net.Socket;
 
 public class TCPNetworkManager {
     public void launchTCPServer() throws IOException {
-        ServerSocket socket = new ServerSocket(4000);
-        Socket link = socket.accept();
+        ServerSocket s = null;
+
+        try {
+            s = new ServerSocket (4000) ;
+            System.out.println("Serveur de socket créé : " + s);
+        }
+        catch (IOException e)
+        {
+            System.err.println ("Erreur socket " + e) ;
+            System.exit (1) ;
+        }
+
+        while(true){ //Sert pour avoir plusieurs clients en même temps
+            System.out.println("En attente de connexion...");
+            Socket service = s.accept() ;//On attend l'arrivée d'un client
+            System.out.println("Client connecté :" + s); //Service est le socket (tuyau de communication) vers le client qui vient de se connecter
+
+            //Pour chaque client connecté on crée un Thread qui va gérer les communications
+            TransmitterThread transmit = new TransmitterThread(service);
+            transmit.start(); //On lance le Thread (--> run() dans TransmitterThread)
+
+            ReceiverThread receive = new ReceiverThread(service);
+            receive.start(); //On lance le Thread (--> run() dans ReceiverThread)
+        }
+
     }
 
 }
--- src/main/java/network/TransmitterThread.java
+++ src/main/java/network/TransmitterThread.java
@@ -1,12 +1,37 @@
 package network;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
 
-public class TransmitterThread {
-    public void startConversation() throws IOException {
-        Socket link = new Socket("127.0.0.1",4000);
-        // .............. //
-        link.close();
+public class TransmitterThread extends Thread {
+
+    private Socket service;
+
+    public TransmitterThread(Socket service) throws IOException {
+        this.service = service; //On mémorise le tuyau de communication
+    }
+
+    public void run() {
+        boolean quit = false;
+        PrintStream flux = null;
+        try {
+            flux = new PrintStream(service.getOutputStream(), true); //J'isole le flux de comm en sortie (serveur vers client)
+
+            while (!quit) { //Tant que le client n'a pas demandé à quitter
+                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+                System.out.println("Enter your message :");
+                String message = myObj.nextLine();  // Read user input
+                flux.println(message);
+                //System.out.println("Username is: " + userName);  // Output user input
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            quit = true;
+        }
     }
 }
