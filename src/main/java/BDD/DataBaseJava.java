43ebfd6f31861841e25e00cd294d3f90dca7fd42
sroche <sgc.roche13@gmail.com>
2023-01-12 10:27:26 +0100

    base de données jdbc

--- src/main/java/BDD/DataBaseJava.java
+++ src/main/java/BDD/DataBaseJava.java
@@ -1,4 +1,33 @@
 package BDD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseJava {
+    public static void connect(){
+        Connection conn = null;
+        try {
+            //db parameters
+            String url = "jdbc:sqlite:messages.db";
+            //create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQL has been established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn!=null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
 }
