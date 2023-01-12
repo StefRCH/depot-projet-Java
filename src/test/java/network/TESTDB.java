dfe97d26baf835f56937d1bc44061c9b693ff48d
sroche <sgc.roche13@gmail.com>
2023-01-12 10:42:43 +0100

    base de données jdbc

--- src/test/java/network/TESTDB.java
+++ src/test/java/network/TESTDB.java
@@ -1,4 +1,17 @@
 package network;
 
+import BDD.DataBaseJava;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
 public class TESTDB {
+
+    @Test
+    public void Connect() throws SQLException{
+        DataBaseJava DB = new DataBaseJava();
+        DB.connect();
+
+        assert true;
+    }
 }
