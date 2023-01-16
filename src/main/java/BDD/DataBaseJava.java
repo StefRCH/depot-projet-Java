db921e8a370a0e2ac9fcc72c4db1b246b57d306d
sroche <sgc.roche13@gmail.com>
2023-01-16 15:35:55 +0100

    base de données jdbc

--- src/main/java/BDD/DataBaseJava.java
+++ src/main/java/BDD/DataBaseJava.java
@@ -26,23 +26,14 @@ public class DataBaseJava {
         }
     }
 
-    public static void addtable(String NomTable){
+    public static void addtable(String NomTable,String Key1,String Key2){
         String url ="jdbc:sqlite:messages.db";
-
         //SQL Statement for new table
-        String sql="CREATE TABLE IF NOT EXISTS "+NomTable;
-
-        String tables="SELECT name FROM sqlite_schema WHERE type='table' ";
+        String sql="CREATE TABLE IF NOT EXISTS "+NomTable+"(id INTEGER PRIMARY KEY AUTOINCREMENT,"+Key1+" text,"+Key2+" text )";
         try{
             Connection conn = DriverManager.getConnection(url);
             Statement stmt = conn.createStatement();
             stmt.execute(sql);
-            ResultSet rs = stmt.executeQuery(tables);
-
-
-            while(rs.next()){
-                System.out.println(rs.getString("name"));
-            }
         } catch(SQLException e){
             System.out.println(e.getMessage());
         }
@@ -77,13 +68,32 @@ public class DataBaseJava {
         }
     }
 
-    public static void addCol(String Nomtable,String Nomcol,String Typecol){
+    public static void Allcol(String Table){
         String url ="jdbc:sqlite:messages.db";
-        String sql ="ALTER TABLE IF EXISTS "+Nomtable+" ADD "+Nomcol+" "+Typecol;
+        System.out.println("Table "+Table+" contient champs :");
+        String col="PRAGMA table_info("+Table+");";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(col);
+            while(rs.next()){
+                System.out.println(rs.getString("name"));
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void addColExt(String Nomtable,String Nomcol,String OtherTable,String OtherCol){
+        String url ="jdbc:sqlite:messages.db";
+        String sql ="ALTER TABLE IF EXISTS "+Nomtable+" ADD FOREIGN KEY ("+Nomcol+") REFERENCES "+OtherTable+" ("+OtherCol+");";
     }
     public static void main(String[] args) {
-        connect();
-
-
+        addtable("message","date","payload");
+        addtable("userconversation","ipsource","ipdestination");
+        Alltables();
+        addColExt("message","ipdest","userconversation","ipdestination");
+        Allcol("message");
+        Allcol("userconversation");
     }
 }
--- src/test/java/network/TESTDB.java
+++ src/test/java/network/TESTDB.java
@@ -1,15 +1,128 @@
 package network;
 
 import BDD.DataBaseJava;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TESTDB {
 
+    @Before
+    public void clearDB(){
+        String url ="jdbc:sqlite:messages.db";
+        String sql="DROP TABLE *";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Test
-    public void addDataTable(){
+    public void testCREATETABLE(){
+        String url ="jdbc:sqlite:messages.db";
+        //SQL Statement for new table
+        String sql="CREATE TABLE IF NOT EXISTS TEST(id INTEGER PRIMARY KEY,bla text)";
+        String tables="SELECT name FROM sqlite_schema WHERE type='table' ";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
 
+            stmt.execute(sql);
+            ResultSet rs = stmt.executeQuery(tables);
+            boolean testf=false;
+            while(rs.next()){
+                String Test=rs.getString("name");
+                if(Test.equals("TEST")){
+                    testf=true;
+                }
+            }
+            assert testf;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
+
+    @Test
+    public void testINSERT(){
+        String url ="jdbc:sqlite:messages.db";
+        //SQL Statement for new table
+        String sql="INSERT INTO TEST(id,bla) VALUES(999,'DWBH')";
+        String result="SELECT * FROM TEST WHERE id=999";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+
+            stmt.execute(sql);
+            ResultSet rs = stmt.executeQuery(result);
+            boolean testf=false;
+            while(rs.next()){
+                String resid=rs.getString("id");
+                String restxt=rs.getString("bla");
+                if(resid.equals("999")&restxt.equals("DWBH")){
+                    testf=true;
+                }
+            }
+            assert testf;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    @Test
+    public void testDELETE(){
+        String url ="jdbc:sqlite:messages.db";
+        //SQL Statement for new table
+        String sql="DELETE FROM TEST WHERE id=999";
+        String result="SELECT * FROM TEST WHERE id=999";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+
+            stmt.execute(sql);
+            ResultSet rs = stmt.executeQuery(result);
+            boolean testf=true;
+            while(rs.next()){
+                String resid=rs.getString("id");
+                String restxt=rs.getString("bla");
+                if(resid.equals("999")&restxt.equals("DWBH")){
+                    testf=false;
+                }
+            }
+            assert testf;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDELETETABLE(){
+        String url ="jdbc:sqlite:messages.db";
+        //SQL Statement for new table
+        String sql="DROP TABLE TEST";
+        String tables="SELECT name FROM sqlite_schema WHERE type='table' ";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+
+            stmt.execute(sql);
+            ResultSet rs = stmt.executeQuery(tables);
+            boolean testf=true;
+            while(rs.next()){
+                String Test=rs.getString("name");
+                if(Test.equals("TEST")){
+                    testf=false;
+                }
+            }
+            assert testf;
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
